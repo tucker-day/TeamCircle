d814b6d51db84f39e3e353ea5dae4f122503dfb3
SethHyland <145570001+SethHyland@users.noreply.github.com>
2025-02-04 16:47:23 -0500

    Started Creation of base stats and gold class

---
+++ Assets/Scripts/Gameplay/BaseStats.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct BaseStuct
+{
+    float MaxHP;//
+    float CurrentHP;// use this stat when modifying the players hp
+    float PlayerSpeed;//default speed
+    float PlayerLuck;//default luck
+}
