f3d2fea605966e4b048e1a122a1fefefbd3f0dd5
tucker-day <daytucker.b@gmail.com>
2025-02-05 21:53:28 -0500

    Create fundamental functions of Dungeon Manager

--- Assets/Scripts/Dungeon/DungeonManager.cs
+++ Assets/Scripts/Dungeon/DungeonManager.cs
@@ -9,18 +9,54 @@ public class DungeonManager : MonoBehaviour
     int branchReduction = 0;
     bool bossRoomSpawned = false;
 
+    private void Start()
+    {
+        GenerateDungeon();
+    }
+
+    // Start the dungeon generation
     public void GenerateDungeon() 
     {
-
+        StartCoroutine(SpawnSpawnRoom());
     }
 
-    public void SpawnSpawnRoom()
+    private IEnumerator SpawnSpawnRoom()
     {
+        GameObject spawnRoom = Instantiate(settings.tileset.spawnRoom);
+        Hallway spawnHall = spawnRoom.GetComponent<ChildRoom>().hallways[0];
 
+        yield return SpawnChildRoom(spawnHall, 0);
     }
 
-    public void SpawnChildRoom(Hallway connect)
+    private IEnumerator SpawnChildRoom(Hallway connect, int distance)
     {
 
+        GameObject childRoom = Instantiate(settings.tileset.spawnRoom, SpawnPointFromHall(connect), Quaternion.identity);
+
+        // Wait for collisions to happen
+        yield return new WaitForFixedUpdate();
+    }
+
+    private Vector3 SpawnPointFromHall(Hallway hall)
+    {
+        Vector3 result = hall.gameObject.transform.position;
+
+        switch(hall.direction)
+        {
+            case HallwayDirection.Up:
+                result.y += settings.tileset.tileSize.y / 2;
+                break;
+            case HallwayDirection.Down:
+                result.y += settings.tileset.tileSize.y / 2;
+                break;
+            case HallwayDirection.Right:
+                result.x += settings.tileset.tileSize.x / 2;
+                break;
+            case HallwayDirection.Left:
+                result.x += settings.tileset.tileSize.x / 2;
+                break;
+        }
+
+        return result;
     }
 }
