b3c39e2f8141e14815ab974432d0e5e185293ec7
tucker-day <daytucker.b@gmail.com>
2025-02-05 20:14:28 -0500

    Create Dungeon Manager
    Basic variables and functions

---
+++ Assets/Scripts/Dungeon/DungeonManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DungeonManager : MonoBehaviour
+{
+    public DungeonSettings settings;
+
+    int branchReduction = 0;
+    bool bossRoomSpawned = false;
+
+    public void GenerateDungeon() 
+    {
+
+    }
+
+    public void SpawnSpawnRoom()
+    {
+
+    }
+
+    public void SpawnChildRoom(Hallway connect)
+    {
+
+    }
+}
