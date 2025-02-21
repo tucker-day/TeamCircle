c8a291fbc344dbb0819c07164b0fa8efdb7a9d7d
tucker-day <daytucker.b@gmail.com>
2025-02-21 11:49:33 -0500

    Created Edge Restrictions for the Child Room Class

--- Assets/Scripts/Dungeon/ChildRoom.cs
+++ Assets/Scripts/Dungeon/ChildRoom.cs
@@ -1,8 +1,22 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
+
+[Serializable]
+public class EdgeRestrictions
+{
+    [field: SerializeField] public bool canBeWall { get; private set; } = true;
+    [field: SerializeField] public bool canBeDoor { get; private set; } = true;
+    [field: SerializeField] public bool canBeOpen { get; private set; } = true;
+}
 
 [RequireComponent(typeof(BoxCollider2D))]
 [RequireComponent(typeof(Rigidbody2D))]
 public class ChildRoom : MonoBehaviour
 {
+    [Header("Edge Restrictions")]
+    public EdgeRestrictions upperEdgeRestrictions = new EdgeRestrictions();
+    public EdgeRestrictions rightEdgeRestrictions = new EdgeRestrictions();
+    public EdgeRestrictions lowerEdgeRestrictions = new EdgeRestrictions();
+    public EdgeRestrictions leftEdgeRestrictions = new EdgeRestrictions();
 }
