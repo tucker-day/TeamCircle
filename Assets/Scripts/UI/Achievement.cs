9fefa8d67865a60556d9085bcbab3c47176a3b1e
bishalsapkota <sapkotabishal00@gmail.com>
2025-03-16 19:46:38 -0400

    Polished Menu
    The main menu is polished, added animations for UI as well as the buttons are highlighted when mouse hovers.

--- Assets/Scripts/UI/Achievement.cs
+++ Assets/Scripts/UI/Achievement.cs
@@ -6,18 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class Achievement : MonoBehaviour
 {
+    FadeInOutSceneAnim fade;
       public Button AchievementButton;
     void Start()
     {
-        
+        fade=FindObjectOfType<FadeInOutSceneAnim>();
         AchievementButton.onClick.AddListener(OnAchievementButtonPressed);
     }
 
+    public IEnumerator ChangeScene(){
+        fade.FadeIn();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Achievement");
+    }
 
      public void OnAchievementButtonPressed()
     {
+        StartCoroutine(ChangeScene());
         
-        SceneManager.LoadScene("Achievement");
     }
 } 
 
--- Assets/Scripts/UI/StartGame.cs
+++ Assets/Scripts/UI/StartGame.cs
@@ -6,19 +6,25 @@ using UnityEngine.UI;
 
 public class StartGame : MonoBehaviour
 {
-    public Button startButton;  
+    public Button startButton; 
+    FadeInOutSceneAnim fade; 
 
     void Start()
     {
-        
+         fade=FindObjectOfType<FadeInOutSceneAnim>();
         startButton.onClick.AddListener(OnStartButtonPressed);
+    }
+     public IEnumerator ChangeScene(){
+        fade.FadeIn();
+        yield return new WaitForSeconds(1);
+         SceneManager.LoadScene("Dungeon");
     }
 
 
      public void OnStartButtonPressed()
     {
         
-        SceneManager.LoadScene("Dungeon");
+       StartCoroutine(ChangeScene());
     }
 }
 
