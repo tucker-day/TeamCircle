2a2f291f7b71d45699c156eb64184a23e7f77691
Ana <anap0704@hotmail.com>
2025-03-16 16:47:07 -0400

    Populated Projectile class

--- Assets/Scripts/Gameplay/Projectile.cs
+++ Assets/Scripts/Gameplay/Projectile.cs
@@ -4,6 +4,24 @@ using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
+    public Rigidbody2D projectileRb;
+    public Transform launchPoint;
+    public float speed;
 
+    public GameObject target;
 
+    public float lifespan;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(gameObject);
+    }
 }
