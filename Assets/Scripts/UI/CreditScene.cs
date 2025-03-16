fa3e283d0b383facb7ad6fc1601021827f4f1c5d
bishalsapkota <sapkotabishal00@gmail.com>
2025-03-16 19:46:38 -0400

    Polished Menu
    The main menu is polished, added animations for UI as well as the buttons are highlighted when mouse hovers.

--- Assets/Scripts/UI/CreditScene.cs
+++ Assets/Scripts/UI/CreditScene.cs
@@ -7,16 +7,22 @@ using UnityEngine.SceneManagement;
 public class CreditScene : MonoBehaviour
 {
     public Button CreditButton;
+    FadeInOutSceneAnim fade;
     void Start()
     {
-        
+        fade=FindObjectOfType<FadeInOutSceneAnim>();
         CreditButton.onClick.AddListener(OnCreditButtonPressed);
     }
 
+public IEnumerator ChangeScene(){
+        fade.FadeIn();
+        yield return new WaitForSeconds(1);
+         SceneManager.LoadScene("Credit");
+    }
 
      public void OnCreditButtonPressed()
     {
-        
-        SceneManager.LoadScene("Credit");
+         StartCoroutine(ChangeScene());
+       
     }
 }
--- Assets/Scripts/UI/SettingScene.cs
+++ Assets/Scripts/UI/SettingScene.cs
@@ -5,19 +5,25 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class SettingScene : MonoBehaviour
-{
+{   
+     FadeInOutSceneAnim fade;
    public Button settingButton;  
 
     void Start()
     {
-        
+        fade=FindObjectOfType<FadeInOutSceneAnim>();
         settingButton.onClick.AddListener(OnSettingButtonPressed);
+    }
+     public IEnumerator ChangeScene(){
+        fade.FadeIn();
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Setting");
     }
 
 
      public void OnSettingButtonPressed()
     {
-        
-        SceneManager.LoadScene("Setting");
+         StartCoroutine(ChangeScene());
+       
     }
 }
