d18cb8e5cbe1892147d7eb0a9d880f7404438b42
Ana <anap0704@hotmail.com>
2025-03-16 17:19:01 -0400

    Added prototype function to move projectile

--- Assets/Scripts/Gameplay/Projectile.cs
+++ Assets/Scripts/Gameplay/Projectile.cs
@@ -1,27 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour
 {
     public Rigidbody2D projectileRb;
     public Transform launchPoint;
+    private Vector2 direction;
     public float speed;
+    public float lifespan = 2f;
 
     public GameObject target;
-
-    public float lifespan;
     void Start()
     {
-        
+        projectileRb = GetComponent<Rigidbody2D>();
     }
     void Update()
     {
         
     }
 
+    // Prototype function
+    void FixedUpdate()
+    {
+        projectileRb.velocity = direction * speed;
+    }
+
     public void OnCollisionEnter2D(Collision2D collision)
     {
+        Debug.Log("Projectile hit " + collision.gameObject.name);
         Destroy(gameObject);
     }
 }
