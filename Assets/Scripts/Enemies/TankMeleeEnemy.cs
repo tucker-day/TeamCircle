b9a852cbc1c78493ddb458e4fbb3ab39d0c047d1
Ana <anap0704@hotmail.com>
2025-03-26 09:15:45 -0400

    Added components to Tank melee prefab

--- Assets/Scripts/Enemies/TankMeleeEnemy.cs
+++ Assets/Scripts/Enemies/TankMeleeEnemy.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TankMeleeEnemy : MeleeEnemy
+public class TankMeleeEnemy : Enemy
 {
     public float distance;
     void Start()
