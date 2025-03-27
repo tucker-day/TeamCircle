3c0b184f6fc27c281abf6bd7b93b5c34ac7e77a9
Frigiduck <bryn.emberley@gmail.com>
2025-03-27 17:20:42 -0400

    Cleaned up code in the fade scene script.
    Made the code in FadeInOutSceneAnim.cs more readable.

--- Assets/Scripts/UI/FadeInOutSceneAnim.cs
+++ Assets/Scripts/UI/FadeInOutSceneAnim.cs
@@ -8,34 +8,33 @@ public class FadeInOutSceneAnim : MonoBehaviour
     public bool fadein = false;
     public bool fadeout = false;
     public float TimeToFade;
-    
 
     // Update is called once per frame
     void Update()
     {
-       if(fadein== true){
-        if(canvasgroup.alpha <1){
-            canvasgroup.alpha +=TimeToFade * Time.deltaTime;
-            if(canvasgroup.alpha>=1){
-                fadein=false;
+        if (fadein)
+        {
+            if (canvasgroup.alpha < 1)
+            {
+                canvasgroup.alpha += TimeToFade * Time.deltaTime;
+                if (canvasgroup.alpha >= 1)
+                { fadein = false; }
             }
-        }
-       } 
-       if(fadeout== true){
-        if(canvasgroup.alpha>=0){
-            canvasgroup.alpha -=TimeToFade * Time.deltaTime;
-            if(canvasgroup.alpha==0){
-                fadeout =false;
+        } 
+        if (fadeout)
+        {
+            if (canvasgroup.alpha >= 0)
+            {
+                canvasgroup.alpha -= TimeToFade * Time.deltaTime;
+                if (canvasgroup.alpha == 0)
+                { fadeout = false; }
             }
-        }
-       } 
+        } 
     }
 
-    public void FadeIn(){
-        fadein=true;
-    }
+    public void FadeIn()
+    { fadein = true; }
 
-    public void FadeOut(){
-        fadeout=true;
-    }
+    public void FadeOut()
+    { fadeout = true; }
 }
