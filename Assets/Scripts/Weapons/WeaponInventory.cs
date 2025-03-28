0fd5fe184fe0435cc5e424695dae0165b1b0218d
tucker-day <daytucker.b@gmail.com>
2025-03-28 12:04:19 -0400

    Added validation to weapon inventory
    Remakes the array if number of weapons is changed and copies over all the data it can

--- Assets/Scripts/Weapons/WeaponInventory.cs
+++ Assets/Scripts/Weapons/WeaponInventory.cs
@@ -15,4 +15,22 @@ public class WeaponInventory : MonoBehaviour
     [SerializeField]
     private Equipment[] weapons = new Equipment[(int)WeaponType.NUM_WEAPONS];
     private float[] cooldowns = new float[(int)WeaponType.NUM_WEAPONS];
+
+    private void OnValidate()
+    {
+        if (weapons.Length != (int)WeaponType.NUM_WEAPONS)
+        {
+            cooldowns = new float[(int)WeaponType.NUM_WEAPONS];
+
+            Equipment[] temp = new Equipment[(int)WeaponType.NUM_WEAPONS];
+            int len = (weapons.Length < (int)WeaponType.NUM_WEAPONS) ? weapons.Length : (int)WeaponType.NUM_WEAPONS;
+
+            for (int i = 0; i < len; i++)
+            {
+                temp[i] = weapons[i];
+            }
+
+            weapons = temp;
+        }
+    }
 }
