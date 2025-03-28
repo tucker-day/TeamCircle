e91b31320ba0bc9c6dce1efe0af99a6c0653a4c1
tucker-day <daytucker.b@gmail.com>
2025-03-28 11:50:00 -0400

    Created sword prefab and script

---
+++ Assets/Scripts/Weapons/Sword.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : Equipment
+{
+    public override void Trigger()
+    {
+        Debug.Log("Boop :3");
+    }
+}
