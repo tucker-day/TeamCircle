2e47f7c3b94c3441a15bab0a22c9abc030b1f20f
SethHyland <145570001+SethHyland@users.noreply.github.com>
2025-04-02 11:43:39 -0400

    worked on fireball and added tags to enemy

--- Assets/Scripts/Weapons/Fireball.cs
+++ Assets/Scripts/Weapons/Fireball.cs
@@ -5,16 +5,15 @@ using UnityEngine;
 public class Fireball : Equipment
 {
     public float rotationSpeed;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z + (rotationSpeed * Time.deltaTime));
+        if(level != 0)
+        {
+
+        }
     }
 
     public override void Trigger(Vector2 playerMovementDir)
