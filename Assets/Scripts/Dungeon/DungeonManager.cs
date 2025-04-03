8d6e81760e454140d373e9dda87e20d15918f8d4
tucker-day <daytucker.b@gmail.com>
2025-04-03 08:45:40 -0400

    Fixed null reference exception caused by flipped if statements

--- Assets/Scripts/Dungeon/DungeonManager.cs
+++ Assets/Scripts/Dungeon/DungeonManager.cs
@@ -530,8 +530,8 @@ public class DungeonManager : MonoBehaviour
 
     private void LinkOpensOnRoom(int x, int y, List<ChildRoom> links = null)
     {
-        if (dungeonGrid[x, y] != null) return;
-        if (dungeonGrid[x, y].childRoom.chainedRooms == null) return;
+        if (dungeonGrid[x, y] == null) return;
+        if (dungeonGrid[x, y].childRoom.chainedRooms != null) return;
 
         if (links == null)
         {
