d218818754fa578c0b04dd62828ac4fc389d68e7
tucker-day <daytucker.b@gmail.com>
2025-04-03 08:50:02 -0400

    De-nested some code for readability

--- Assets/Scripts/Dungeon/DungeonManager.cs
+++ Assets/Scripts/Dungeon/DungeonManager.cs
@@ -543,18 +543,14 @@ public class DungeonManager : MonoBehaviour
 
         foreach (Edges edge in Enum.GetValues(typeof(Edges)))
         {
-            if (dungeonGrid[x, y].GetEdgeType(edge) == EdgeType.Open)
-            {
-                Vector2Int dif = RoomData.GetEdgeVectorConversion(edge);
-                RoomData neighbor = dungeonGrid[x + dif.x, y + dif.y];
+            if (dungeonGrid[x, y].GetEdgeType(edge) != EdgeType.Open) continue;
 
-                if (neighbor != null)
-                {
-                    if (!links.Contains(neighbor.childRoom))
-                    {
-                        LinkOpensOnRoom(x + dif.x, y + dif.y, links);
-                    }
-                }
+            Vector2Int dif = RoomData.GetEdgeVectorConversion(edge);
+            RoomData neighbor = dungeonGrid[x + dif.x, y + dif.y];
+
+            if (neighbor != null && !links.Contains(neighbor.childRoom))
+            {
+                LinkOpensOnRoom(x + dif.x, y + dif.y, links);
             }
         }
     }
