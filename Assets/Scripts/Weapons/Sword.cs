96710a5997a7a5254052457a2c5785d534f2647c
tucker-day <daytucker.b@gmail.com>
2025-04-03 09:47:42 -0400

    Created projectile spawning functionality for sword

--- Assets/Scripts/Weapons/Sword.cs
+++ Assets/Scripts/Weapons/Sword.cs
@@ -4,10 +4,40 @@ using UnityEngine;
 
 public class Sword : Equipment
 {
-    [SerializeField] private SwordProjectile swordProjectilePrefab;
+    [SerializeField] 
+    private SwordProjectile swordProjectilePrefab;
 
+    [SerializeField]
+    private int initialDamage;
+    [SerializeField]
+    private int damagePerLevel;
+
+    private bool flipSword = false;
+    
     public override void Trigger(Vector2 playerMovementDir)
     {
-        Debug.Log("Sword Attack In Direction: " + playerMovementDir);
+        if (playerMovementDir.x != 0)
+        {
+            flipSword = playerMovementDir.x < 0;
+        }
+
+        SpawnProjectile(flipSword, Vector3.zero);
+    }
+
+    private void SpawnProjectile(bool flip, Vector3 offset)
+    {
+        swordProjectilePrefab.damage = GetDamage();
+        GameObject instance = Instantiate(swordProjectilePrefab.gameObject, transform.position + offset, Quaternion.identity, gameObject.transform);
+
+        if (flip)
+        {
+            Vector3 scale = instance.transform.localScale;
+            instance.transform.localScale = new Vector3(scale.x * -1, scale.y, scale.z);
+        }
+    }
+
+    private int GetDamage()
+    {
+        return initialDamage + damagePerLevel * level;
     }
 }
