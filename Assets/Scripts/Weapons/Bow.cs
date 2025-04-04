1ad2fdf621ce1f4c916b14e90f945c594cdf8ddf
tucker-day <daytucker.b@gmail.com>
2025-04-04 14:25:10 -0400

    Changed some bow stats

--- Assets/Scripts/Weapons/Bow.cs
+++ Assets/Scripts/Weapons/Bow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bow : Equipment
 {
-    const float SPAWN_DELAY = 0.25f;
+    const float SPAWN_DELAY = 0.1f;
 
     [SerializeField]
     private BowProjectile bowProjectilePrefab;
