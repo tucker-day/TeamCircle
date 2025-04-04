f7d05a32e48a76fe95936e604b0c5957dc7d225d
tucker-day <daytucker.b@gmail.com>
2025-04-04 14:10:20 -0400

    Created bow projectile functionality

--- Assets/Scripts/Weapons/BowProjectile.cs
+++ Assets/Scripts/Weapons/BowProjectile.cs
@@ -3,19 +3,28 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class BowProjectile : MonoBehaviour
 {
+    const float LIFETIME = 10.0f;
+
     [HideInInspector]
     public int damage;
     [HideInInspector]
     public float speed;
 
-    private void Start()
+    public float endTime;
+
+    public void Start()
     {
-        
+        endTime = Time.time + LIFETIME;
     }
 
     private void FixedUpdate()
     {
-        
+        transform.position += speed * Time.fixedDeltaTime * transform.right;
+
+        if (Time.time > endTime)
+        {
+            Destroy(gameObject);
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -23,6 +32,7 @@ public class BowProjectile : MonoBehaviour
         if (collision.gameObject.TryGetComponent(out Enemy enemy))
         {
             enemy.TakeDamage(damage);
+            Destroy(gameObject);
         }
     }
 }
