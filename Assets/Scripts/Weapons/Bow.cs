100cf49227cea786ab724d04a511fd13c4dcd692
tucker-day <daytucker.b@gmail.com>
2025-04-04 13:02:14 -0400

    Changed spawn delay

--- Assets/Scripts/Weapons/Bow.cs
+++ Assets/Scripts/Weapons/Bow.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bow : Equipment
 {
-    const float SPAWN_DELAY = 0.2f;
+    const float SPAWN_DELAY = 0.1f;
 
     [SerializeField]
     private BowProjectile bowProjectile;
