d94a278e0d7a70fa049fc3b3d18ac5f3ea452aeb
Ana <anap0704@hotmail.com>
2025-04-09 08:47:59 -0400

    Deleted unused function that checks for projectile tag

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -100,13 +100,6 @@ void Update()
         currentState.EnterState(this);
     }
 
-    /*public void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.gameObject.tag == "Projectile")
-        {
-            TakeDamage(damage);
-        }
-    }*/
     public virtual void Move() { }
 
     public virtual void Chase() { }
