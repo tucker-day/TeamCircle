db088c4636a6c1e1158a6625e1fd475f19d18a8a
tucker-day <daytucker.b@gmail.com>
2025-04-09 09:22:54 -0400

    Created a minimap camera and render texture

---
+++ Assets/Scripts/UI/MinimapCamera.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.Universal;
+
+public class MinimapCamera : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        
+    }
+}
