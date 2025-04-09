a9121004b9d911d9b73a65d189b6f1ccfdc33967
SethHyland <145570001+SethHyland@users.noreply.github.com>
2025-04-09 15:55:22 -0400

    Ice spell mostly works

---
+++ Assets/Scripts/Weapons/IceSpike.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IceSpike : Equipment
+{
+    const float SPAWN_DELAY = 0.1f;
+
+    [SerializeField]
+    private IceSpikeProjectile IceSpikeProjectilePrefab;
+
+    [SerializeField]
+    private int initialDamage;
+    [SerializeField]
+    private int damagePerLevel;
+
+    [SerializeField]
+    private float initialSpeed;
+    [SerializeField]
+    private float speedPerLevel;
+
+    [SerializeField]
+    private float cooldownPerLevel;
+
+    [SerializeField]
+    private List<int> projectileIncreaseThresholds;
+
+    [SerializeField]
+    private int rotationSpeed;
+
+    public int BounceCount;
+
+    public override void Trigger(Vector2 playerMovementDir)
+    {
+        SpawnProjectile(playerMovementDir);
+    }
+    private void Update()
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, transform.rotation.eulerAngles.z + (rotationSpeed * Time.deltaTime));
+    }
+
+    private void SpawnProjectile(Vector2 direction)
+    {
+        IceSpikeProjectilePrefab.damage = GetDamage();
+        IceSpikeProjectilePrefab.speed = GetSpeed();
+
+        GameObject instance = Instantiate(IceSpikeProjectilePrefab.gameObject, transform.position, Quaternion.identity);
+
+        float angle = Vector2.Angle(Vector2.right, direction);
+        if (direction.y < 0)
+        {
+            angle *= -1;
+        }
+        instance.transform.rotation = Quaternion.Euler(0, 0, angle);
+    }
+
+    private int GetDamage()
+    {
+        return initialDamage + damagePerLevel * level;
+    }
+
+    private float GetSpeed()
+    {
+        return initialSpeed + speedPerLevel * level;
+    }
+
+    public override void LevelUp()
+    {
+        base.LevelUp();
+        Debug.Log("Ice Spike is now Level" + level);
+    }
+}
+
