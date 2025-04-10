379ad0f7e65397c0f8ee455083b6118db4ba8fa7
Nina2515 <valentina.cossiolondono@student.sl.on.ca>
2025-04-10 16:15:07 -0400

    Adding arrow

--- Assets/Scripts/Chest/ChestScript.cs
+++ Assets/Scripts/Chest/ChestScript.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
+
+                          //Interactable
 public class ChestScript : MonoBehaviour
 {
+
+    //public Item contents; 
+    public bool ChestOpen;
+    //public Signal raiseItem;
+
     // Start is called before the first frame update
     void Start()
     {
