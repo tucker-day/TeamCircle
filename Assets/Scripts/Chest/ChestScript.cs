c8400021e3a0f566658f775dca04235d3f2ffb8a
Nina2515 <valentina.cossiolondono@student.sl.on.ca>
2025-04-16 09:04:11 -0400

    Fixed bugs

--- Assets/Scripts/Chest/ChestScript.cs
+++ Assets/Scripts/Chest/ChestScript.cs
@@ -1,28 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI; 
+using UnityEngine.UI;
 
-
-                          //Interactable
 public class ChestScript : MonoBehaviour
+
 {
+    public bool ChestOpen; 
 
-    //public Item contents; 
-    public bool ChestOpen;
-    //public Signal raiseItem;
-
-    // Start is called before the first frame update
-    void Start()
+     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+     void Update()
     {
-        
+       
     }
+
+   
+
+    
 
     public void OnTriggerEnter2D(Collider2D other)
     {
